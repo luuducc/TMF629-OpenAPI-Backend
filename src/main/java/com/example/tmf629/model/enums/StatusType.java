8367b6281deba1c6ab233459f593bf67bc572c97
luuduc <luuduc1708@gmail.com>
2025-05-16 11:51:04 +0700

    fix: make enum StatusType back to number style

--- src/main/java/com/example/tmf629/model/enums/StatusType.java
+++ src/main/java/com/example/tmf629/model/enums/StatusType.java
@@ -1,25 +1,15 @@
 package com.example.tmf629.model.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum StatusType {
-    Draft(0)     ,     // secondary
-    Created(1),        // info
-    Pending(2),        // info
-    Verified(3),       // success
-    Active(4),         // success
-    Suspended(5),      // warn
-    Inactive(6),       // secondary
-    Archived(7),       // secondary
-    Blacklisted(8),    // danger
-    Rejected(9),       // danger
-    Deleted(10);
-    private final int code;
-    StatusType(int code) {
-        this.code = code;
-    }
-    @JsonValue
-    public int getCode() {
-        return code;
-    }
+    Draft,         // secondary
+    Created,       // info
+    Pending,       // info
+    Verified,      // success
+    Active,        // success
+    Suspended,     // warn
+    Inactive,      // secondary
+    Archived,      // secondary
+    Blacklisted,   // danger
+    Rejected,      // danger
+    Deleted        // danger
 }
